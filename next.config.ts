71b37937edcef0c4556ef9ef1e493c5337494e24
Suwijak Promsatid <suwijak.pak@gmail.com>
2025-10-27 09:34:32 +0700

    refactor: Add Next

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,24 @@
 import type { NextConfig } from "next";
+import withPWA from "next-pwa";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  images: { remotePatterns: [{
-    protocol: 'https',
-    hostname: 'cdn.discordapp.com',
-    port: '',
-    pathname: '/**',
-  },] },
+  images: {
+    remotePatterns: [{
+      protocol: 'https',
+      hostname: 'cdn.discordapp.com',
+      port: '',
+      pathname: '/**',
+    },]
+  },
 };
 
-export default nextConfig;
+export default withPWA({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
+  fallbacks: {
+    document: "/offline",
+  },
+})(nextConfig);
