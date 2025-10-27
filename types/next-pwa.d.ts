cfbf2f57065546b0399e8706f3ca21411932d5a7
Suwijak Promsatid <suwijak.pak@gmail.com>
2025-10-27 09:34:32 +0700

    feat(types): Add type definitions for next-pwa

---
+++ types/next-pwa.d.ts
@@ -0,0 +1,22 @@
+declare module 'next-pwa' {
+  import { NextConfig } from 'next';
+
+  interface PWAConfig {
+    dest?: string;
+    register?: boolean;
+    skipWaiting?: boolean;
+    disable?: boolean;
+    sw?: string;
+    scope?: string;
+    reloadOnOnline?: boolean;
+    fallbacks?: {
+      document?: string;
+      image?: string;
+      audio?: string;
+      video?: string;
+      font?: string;
+    };
+  }
+
+  export default function withPWA(config: PWAConfig): (nextConfig: NextConfig) => NextConfig;
+}
